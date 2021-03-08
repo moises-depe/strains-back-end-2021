0a6445b7fcb9f78d197f66ab5909bf425732d032
moises depena <swe.moisesdepe@gmail.com>
2021-03-08 10:46:03 -0800

    UpdatedTable

---
+++ data/favorites.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    name: 'cool train',
+    race: 'hybrid',
+    flavors: ['earthy', 'pine'],
+    positive: ['relaxed', 'happy', 'sleepy'],
+    negative: ['dizzy'],
+    medical: ['stress', 'insomnia', 'depression'],
+    description: 'Bakerstreet is a variety of Hindu Kush grown by Canadian LP Tweed. It is a pure indica with origins in the Hindu Kush mountain range. The subtle sweet and earthy sandalwood aroma of Bakerstreet induces a deep sense of calm that helps bring relief to those suffering pain, nausea, and stress disorders. Its heavy body effects make it a top strain to help you relax and unwind at the end of a long day. '
+
+  },
+  {
+    name: 'happy times',
+    race: 'indica',
+    flavors: ['Spicy/Herbal', 'Pungent', 'Earthy'],
+    positive: ['relaxed', 'happy', 'sleepy'],
+    negative: ['dizzy', 'drymouth'],
+    medical: ['stress', 'insomnia', 'depression'],
+    description: 'Alaskan Ice by Green House Seeds is a powerful sativa that crosses a euphoric White Widow hybrid with the energizing buzz of Haze. Frostlike resin blankets the buds in a promise of soaring psychoactivity, anchored only by its moderate CBD content. The intensity of this 70% sativa strain is recommended for evening consumption and unproductive weekends. Alaskan Ice is a slight variant of Moby Dick, but poses a greater challenge to growers; cultivators with the expertise to raise Alaskan Ice will be rewarded with a highly potent harvest of sour, spicy buds following a 9 week flowering period. The high resin content of Alaskan Ice has made this strain a favorite among hash producers and patients with severe symptoms."'
+  },
+  {
+    name: 'african',
+    race: 'sativa',
+    flavors: ['earthy', 'pine'],
+    positive: ['relaxed', 'happy', 'sleepy'],
+    negative: ['dizzy'],
+    medical: ['stress', 'insomnia', 'depression'],
+    description: 'African refers to the indigenous varieties of cannabis (or landraces) that grow natively in this region of the world. Because of this region\'s latitude and climate, these native landrace strains tend to be sativa in structure and effect. '
+  }
+];
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const favorites = require('./favorites.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -18,30 +18,30 @@ async function run() {
                       VALUES ($1, $2)
                       RETURNING *;
                   `,
-        [user.email, user.hash]);
+          [user.email, user.hash]);
       })
     );
-      
+
     const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      favorites.map(favorite => {
         return client.query(`
-                    INSERT INTO animals (name, cool_factor, owner_id)
-                    VALUES ($1, $2, $3);
+                    INSERT INTO favorites (name, race, flavors, positive, negative, medical, description, owner_id)
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
                 `,
-        [animal.name, animal.cool_factor, user.id]);
+          [favorite.name, favorite.race, favorite.flavors, favorite.positive, favorite.negative, favorite.medical, favorite.description, user.id]);
       })
     );
-    
+
 
     console.log('seed data load complete', getEmoji(), getEmoji(), getEmoji());
   }
-  catch(err) {
+  catch (err) {
     console.log(err);
   }
   finally {
     client.end();
   }
-    
+
 }
