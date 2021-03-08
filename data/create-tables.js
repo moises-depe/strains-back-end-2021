6eeddb92ce3591930f7e50a6ab839f4b0868d114
phsequeira <phsequeira@yahoo.com>
2021-03-08 09:53:38 -0800

    started be

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,15 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE favorites (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    race VARCHAR(512) NOT NULL,
+                    flavors ARRAY NOT NULL,
+                    positive ARRAY NOT NULL,
+                    negative ARRAY NOT NULL,
+                    medical ARRAY NOT NULL,
+                    description VARCHAR(512) NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
